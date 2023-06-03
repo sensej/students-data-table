9ab195a3e0f2af285abc7a1660b9474f3cbb56cb
Ramazan Vlilyaev <itsensej@gmail.com>
2023-06-03 13:32:41 +0300

    Реализовано сохранение данных из таблицы в csv файл

---
+++ src/js/exportTableToCSV.js
@@ -0,0 +1,32 @@
+export default function exportTableToCSV(filename) {
+  let csv = [];
+  let rows = document.querySelectorAll('table tr');
+
+  for (let i = 0; i < rows.length; i++) {
+    let row = [];
+    let cols = rows[i].querySelectorAll('td, th');
+
+    for (let j = 0; j < cols.length; j++) {
+      row.push(cols[j].innerText);
+    }
+
+    csv.push(row.join(','));
+  }
+
+  console.log(csv.join('\n'));
+
+  downloadCSV(csv.join('\n'), filename);
+}
+
+function downloadCSV(csv, filename) {
+  let csvFile = new Blob([csv], {
+    type: 'text/csv',
+  });
+
+  let downloadLink = document.createElement('a');
+  downloadLink.download = filename;
+  downloadLink.href = window.URL.createObjectURL(csvFile);
+  downloadLink.style.display = 'none';
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+}
